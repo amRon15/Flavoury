93309c7f7c935c9932e42c307d4b77b976e8a30f
amRon15 <hallol00111@gmail.com>
2024-03-17 19:04:41 +0800

    style fragment_home.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.flavoury"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
