7d5f428db23d8b693867aabbb117ab0a308b5577
amRon15 <hallol00111@gmail.com>
2024-03-19 02:13:31 +0800

    inital login page, add google authentication, category recyclerView in homepage

---
+++ app/src/main/java/com/example/flavoury/ui/home/HomeAdapter.java
@@ -0,0 +1,57 @@
+package com.example.flavoury.ui.home;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flavoury.R;
+
+public  class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>{
+
+    String[] categoryType;
+
+    public HomeAdapter(String[] categoryType) {
+        this.categoryType = categoryType;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_category_btn_list,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.button.setText(categoryType[position]);
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.setSelected(true);
+                holder.button.setTextColor(Color.WHITE);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryType.length;
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public Button button;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            button = itemView.findViewById(R.id.home_category_btn);
+
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/flavoury/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/flavoury/ui/home/HomeFragment.java
@@ -1,20 +1,28 @@
 package com.example.flavoury.ui.home;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.flavoury.R;
 import com.example.flavoury.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    RecyclerView recyclerView;
+    String[] categoryType;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,9 +31,14 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        recyclerView = root.findViewById(R.id.home_catList);
+        categoryType = getResources().getStringArray(R.array.category);
 
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        HomeAdapter homeAdapter = new HomeAdapter(categoryType);
+        recyclerView.setAdapter(homeAdapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
+
         return root;
     }
 
