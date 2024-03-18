41b203c134a7c6bf7aa470165fd207245bd8356e
TobyFish06 <tobyfish06@gmail>
2024-03-18 15:29:24 +0800

    make searchview...

---
+++ app/src/main/java/com/example/flavoury/ui/search/SearchActivity.java
@@ -0,0 +1,28 @@
+package com.example.flavoury.ui.search;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.flavoury.R;
+
+/*public class SearchActivity extends AppCompatActivity {
+    ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_search);
+        imageButton = findViewById(R.id.imageButton3);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivities(new Intent(SearchActivity.this,));
+            }
+        });
+    }
+}*/
